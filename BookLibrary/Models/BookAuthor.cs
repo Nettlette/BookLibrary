1eb9b40ee1e120f26987d6439bc6911d2ab24189
Carl Nettles <venoym@yahoo.com>
2022-07-17 01:36:01 -0400

    Book Edit page

--- BookLibrary/Models/BookAuthor.cs
+++ BookLibrary/Models/BookAuthor.cs
@@ -7,5 +7,10 @@
         public int BookId { get; set; }
         public Author Author { get; set; }
         public int AuthorId { get; set; }
+
+        public bool Compare (BookAuthor ba)
+        {
+            return ba.AuthorId == this.AuthorId && ba.BookId == this.BookId;
+        }
     }
 }
--- BookLibrary/Pages/Book/Edit.cshtml.cs
+++ BookLibrary/Pages/Book/Edit.cshtml.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using BookLibrary.Data;
 using BookLibrary.Models;
+using BookLibrary.Extensions;
 
 namespace BookLibrary.Pages.Book
 {
@@ -22,6 +23,17 @@ namespace BookLibrary.Pages.Book
 
         [BindProperty]
         public BookLibrary.Models.Book Book { get; set; } = default!;
+        public SelectList Category { get; set; }
+        public SelectList Authors { get; set; }
+        public SelectList Locations { get; set; }
+        public SelectList Subcategories { get; set; }
+        public SelectList Series { get; set; }
+        [BindProperty]
+        public int[]? SelectedAuthors { get; set; }
+        [BindProperty]
+        public int[]? SelectedLocations { get; set; }
+        [BindProperty]
+        public int[]? SelectedSubcategories { get; set; }
 
         public async Task<IActionResult> OnGetAsync(int? id)
         {
@@ -35,6 +47,11 @@ namespace BookLibrary.Pages.Book
             {
                 return NotFound();
             }
+            Category = new SelectList(PopulateDropdowns.GetCategories(), "Value", "Text");
+            Authors = new SelectList(PopulateDropdowns.GetAuthors(_context), "Value", "Text");
+            Locations = new SelectList(PopulateDropdowns.GetLocations(_context), "Value", "Text");
+            Subcategories = new SelectList(PopulateDropdowns.GetSubcategories(_context), "Value", "Text");
+            Series = new SelectList(PopulateDropdowns.GetSeries(_context), "Value", "Text");
             Book = book;
             return Page();
         }
@@ -52,6 +69,52 @@ namespace BookLibrary.Pages.Book
 
             try
             {
+                // Authors update
+                if (SelectedAuthors != null)
+                {
+                    var oldItems = _context.BookAuthors.Where(x => x.BookId == Book.BookId).ToList();
+                    var newItems = SelectedAuthors.Select(x => new BookAuthor { BookId = Book.BookId, AuthorId = x }).ToList();
+                    var sameItems = oldItems.Where(x => SelectedAuthors.Contains(x.AuthorId)).ToList();
+                    var removedItems = oldItems;
+                    removedItems.RemoveAll(x => sameItems.Contains(x));
+                    foreach(var a in sameItems)
+                    {
+                        newItems.Remove(newItems.First(x => x.AuthorId == a.AuthorId && x.BookId == a.BookId));
+                    }
+                    _context.BookAuthors.AddRange(newItems);
+                    _context.BookAuthors.RemoveRange(removedItems);
+                }
+                // Locations
+                if (SelectedLocations != null)
+                {
+                    var oldItems = _context.BookLocations.Where(x => x.BookId == Book.BookId).ToList();
+                    var newItems = SelectedLocations.Select(x => new BookLocation { BookId = Book.BookId, LocationId = x }).ToList();
+                    var sameItems = oldItems.Where(x => SelectedLocations.Contains(x.LocationId)).ToList();
+                    var removedItems = oldItems;
+                    removedItems.RemoveAll(x => sameItems.Contains(x));
+                    foreach (var a in sameItems)
+                    {
+                        newItems.Remove(newItems.First(x => x.LocationId == a.LocationId && x.BookId == a.BookId));
+                    }
+                    _context.BookLocations.AddRange(newItems);
+                    _context.BookLocations.RemoveRange(removedItems);
+                }
+                // Subcategories
+                if (SelectedSubcategories != null)
+                {
+                    var oldItems = _context.BookSubcategories.Where(x => x.BookId == Book.BookId).ToList();
+                    var newItems = SelectedSubcategories.Select(x => new BookSubcategory { BookId = Book.BookId, SubcategoryId = x }).ToList();
+                    var sameItems = oldItems.Where(x => SelectedSubcategories.Contains(x.SubcategoryId)).ToList();
+                    var removedItems = oldItems;
+                    removedItems.RemoveAll(x => sameItems.Contains(x));
+                    foreach (var a in sameItems)
+                    {
+                        newItems.Remove(newItems.First(x => x.SubcategoryId == a.SubcategoryId && x.BookId == a.BookId));
+                    }
+                    _context.BookSubcategories.AddRange(newItems);
+                    _context.BookSubcategories.RemoveRange(removedItems);
+                }
+
                 await _context.SaveChangesAsync();
             }
             catch (DbUpdateConcurrencyException)
