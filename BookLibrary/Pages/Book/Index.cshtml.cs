0f6b8cbdd446de8aa79d85484969f2f5b8012e37
cenettles <52330722+Nettlette@users.noreply.github.com>
2022-07-18 17:16:56 -0400

    Book Search start

--- BookLibrary/Pages/Book/Index.cshtml.cs
+++ BookLibrary/Pages/Book/Index.cshtml.cs
@@ -20,12 +20,61 @@ namespace BookLibrary.Pages.Book
         }
 
         public List<BookLibrary.Models.BookIndex> Books { get;set; } = default!;
-
+        [BindProperty(SupportsGet = true)]
+        public string? SearchTitle { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string? SearchAuthor { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public DateTime? PublishStartDate { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public DateTime? PublishEndDate { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string[] SearchLocation { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string[] SearchSubcategory { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string? SearchSeries { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public Category? SearchCategory { get; set; }
+        
         public async Task OnGetAsync()
         {
             if (_context.BookIndex != null)
             {
-                Books = await _context.BookIndex.OrderBy(x => x.Title).ToListAsync();
+                IQueryable<BookIndex> BookSearch = _context.BookIndex;
+                if (!String.IsNullOrEmpty(SearchTitle))
+                {
+                    BookSearch = BookSearch.Where(x => x.Title.Contains(SearchTitle));
+                }
+                if (!String.IsNullOrEmpty(SearchAuthor))
+                {
+                    BookSearch = BookSearch.Where(x => x.Authors.Contains(SearchAuthor) || x.Authors == null);
+                }
+                if (PublishStartDate != null)
+                {
+                    BookSearch = BookSearch.Where(x => x.Published >= PublishStartDate);
+                }
+                if (PublishEndDate != null)
+                {
+                    BookSearch = BookSearch.Where(x => x.Published <= PublishEndDate);
+                }
+                //if (!String.IsNullOrEmpty(SearchLocation))
+                //{
+                //    BookSearch = BookSearch.Where(x => x.Locations.Contains(SearchLocation) || x.Locations == null);
+                //}
+                //if (!String.IsNullOrEmpty(SearchSubcategory))
+                //{
+                //    BookSearch = BookSearch.Where(x => x.Subcategories.Contains(SearchSubcategory) || x.Subcategories == null);
+                //}
+                if (!String.IsNullOrEmpty(SearchSeries))
+                {
+                    BookSearch = BookSearch.Where(x => x.SeriesDisplay.Contains(SearchSeries) || x.SeriesDisplay == null);
+                }
+                if (SearchCategory != null)
+                {
+                    BookSearch = BookSearch.Where(x => x.Category == SearchCategory || x.Category == null);
+                }
+                Books = await BookSearch.ToListAsync();
             }
         }
     }
