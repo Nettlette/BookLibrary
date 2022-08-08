c925e1cce5e4370b32ae3ba0d699b304cd62e190
cenettles <52330722+Nettlette@users.noreply.github.com>
2022-08-08 11:11:06 -0400

    Challenges

---
+++ BookLibrary/Migrations/20220805180947_ChallengeSubs.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookLibrary.Migrations
+{
+    public partial class ChallengeSubs : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ChallengeSubscription",
+                columns: table => new
+                {
+                    ChallengeSubscriptionId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ChallengeId = table.Column<int>(type: "int", nullable: false),
+                    ReaderId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ChallengeSubscription", x => x.ChallengeSubscriptionId);
+                    table.ForeignKey(
+                        name: "FK_ChallengeSubscription_Challenge_ChallengeId",
+                        column: x => x.ChallengeId,
+                        principalTable: "Challenge",
+                        principalColumn: "ChallengeId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ChallengeSubscription_Readers_ReaderId",
+                        column: x => x.ReaderId,
+                        principalTable: "Readers",
+                        principalColumn: "ReaderID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ChallengeSubscription_ChallengeId",
+                table: "ChallengeSubscription",
+                column: "ChallengeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ChallengeSubscription_ReaderId",
+                table: "ChallengeSubscription",
+                column: "ReaderId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ChallengeSubscription");
+        }
+    }
+}
